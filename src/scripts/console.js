d89376a3d10815506c41146ff34d5666a8ca1788
Bas Wilson <bas.wilson@dotcontrol.com>
2019-12-11 15:11:02 +0100

    Console can now write lines

--- src/scripts/console.js
+++ src/scripts/console.js
@@ -1,5 +1,127 @@
 export class ConsolePortfolio {
     constructor() {
-        console.log('Console created');
+        this.con = document.querySelector('.js-console');
+        this.verticalIndicator = document.querySelector('.js-indicator-v');
+        this.horizontalIndicator = document.querySelector('.js-indicator-h');
+        this.lineHeight = 26;
+        this.characterWidth = 11;
+
+        this.currentLine = {
+            x: 0,
+            y: 0
+        };
+
+        this.lines = [];
+
+        this.updateIndicator();
+        window.addEventListener("resize", () => {
+            this.updateIndicator();
+        });
+    }
+
+    /**
+     * Updates the location of the indicator according to currentLine
+     */
+    updateIndicator() {
+        this.verticalIndicator.style.top = parseInt(this.lineHeight + (this.currentLine.y * this.lineHeight)) + "px";
+
+        const line = document.querySelector(`#line-${this.lines.length - 1}`);
+        if (line) {
+            // Get the x and y postion of last span element in the line.
+            const last = document.querySelector(`#line-${this.lines.length - 1}`).lastElementChild;
+            const coords = {
+                x: last.offsetLeft,
+                y: last.offsetTop
+            };
+            if (coords.x == 35) return;
+            this.horizontalIndicator.style.left = this.characterWidth + coords.x + "px";
+            this.horizontalIndicator.style.top = coords.y + "px";
+        }
+    }
+
+    /**
+     * Will render a new line and then use the TypeWriter class to write out text
+     * @param {String} string 
+     */
+    writeLine(string, finishedCallback = () => { }) {
+        if (!string) return;
+        this.renderLine();
+
+        new TypeWriter(`#line-${this.lines.length - 1}`, string, {
+            currentCharacterCallback: this.wroteChar,
+            doneCallback: finishedCallback
+        });
+    }
+
+    readLine() {
+
+    }
+
+    /**
+     * Callback function for the TypeWriter
+     * @param {Number} count 
+     */
+    wroteChar(count) {
+        window.con.currentLine.x = count;
+        window.con.updateIndicator();
+    }
+
+    /**
+     * Appends an input or a paragraph to the next line
+     * @param {Bool} input 
+     */
+    renderLine(input = false) {
+        let line;
+
+        if (!input) {
+            const p = document.createElement('p');
+            p.id = `line-${this.lines.length}`;
+            p.classList.add('window__console-line');
+            line = p;
+        } else {
+
+        }
+
+        this.currentLine.y = this.lines.length;
+        this.lines.push(line);
+        this.con.appendChild(line);
+    }
+}
+
+class TypeWriter {
+    constructor(selector, string = [], options = {}) {
+        if (!selector)
+            return console.warn('You need to enter a valid selector');
+        this.element = document.querySelector(selector);
+
+        if (string.length == 0)
+            return console.warn('Your string is empty');
+
+        this.string = string;
+        this.timeBetweenChar = options.hasOwnProperty('timeBetweenChar') ? options.timeBetweenChar : 80;
+        this.timeBetweenWords = options.hasOwnProperty('timeBetweenWords') ? options.timeBetweenWords : 110;
+        this.currentCharacterCallback = options.hasOwnProperty('currentCharacterCallback') ? options.currentCharacterCallback : () => { };
+        this.doneCallback = options.hasOwnProperty('doneCallback') ? options.doneCallback : () => { };
+
+        this.count = 0;
+
+        this.nextChar();
+    }
+
+    nextChar() {
+        if (this.count == this.string.length)
+            return this.doneCallback();
+
+        let timeout = this.timeBetweenChar;
+
+        if (this.string[this.count] == " ")
+            timeout = this.timeBetweenWords;
+
+        setTimeout(() => {
+            this.element.innerHTML += `<span>${this.string[this.count]}</span>`;
+            this.count++;
+            this.currentCharacterCallback(this.count);
+            this.nextChar();
+        }, timeout);
     }
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,4 +3,21 @@ import '../styles/console.scss';
 
 import { ConsolePortfolio } from './console.js';
 
-new ConsolePortfolio();
+window.con = new ConsolePortfolio();
+
+function intro() {
+    let lines = [
+        `Hello and welcome to my ✨ awesome ✨ portfolio console window!`,
+        'I thought it would be cool to show you around in the natural habitat of a developer.\n',
+        'Press any key to continue...'
+    ];
+    window.con.writeLine(lines[0], () => {
+        window.con.writeLine(lines[1], () => {
+            window.con.writeLine(lines[2], () => {
+
+            });
+        });
+    });
+}
+
+intro();
